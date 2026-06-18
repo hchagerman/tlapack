5331ceb561a04a4a027e0e8746ec018fdcf01ba6
hch <hchagerm1@msudenver.edu>
2026-06-18 12:48:45 -0600

    larft_recusion

---
+++ include/tlapack/lapack/larft_recursive.hpp
@@ -0,0 +1,136 @@
+/// @file larft.hpp Forms the triangular factor T of a block reflector.
+/// @author Weslley S Pereira, University of Colorado Denver, USA
+/// @note Adapted from @see
+/// https://github.com/langou/latl/blob/master/include/larft.h
+//
+// Copyright (c) 2025, University of Colorado Denver. All rights reserved.
+//
+// This file is part of <T>LAPACK.
+// <T>LAPACK is free software: you can redistribute it and/or modify it under
+// the terms of the BSD 3-Clause license. See the accompanying LICENSE file.
+
+#ifndef TLAPACK_LARFT_HH
+#define TLAPACK_LARFT_HH
+
+#include "tlapack/base/utils.hpp"
+#include "tlapack/blas/gemm.hpp"
+#include "tlapack/blas/gemv.hpp"
+#include "tlapack/blas/trmv.hpp"
+
+namespace tlapack {
+
+/** Forms the triangular factor T of a block reflector H of order n,
+ * which is defined as a product of k elementary reflectors.
+ *
+ * If direction = Direction::Forward,  H = H_1 H_2 ... H_k and T is upper
+ * triangular. If direction = Direction::Backward, H = H_k ... H_2 H_1 and T is
+ * lower triangular.
+ *
+ * If storeMode = StoreV::Columnwise, the vector which defines the elementary
+ * reflector H(i) is stored in the i-th column of the array V, and
+ *
+ *               H  =  I - V * T * V'
+ *
+ * The shape of the matrix V and the storage of the vectors which define
+ * the H(i) is best illustrated by the following example with n = 5 and
+ * k = 3. The elements equal to 1 are not stored. The rest of the
+ * array is not used.
+ *
+ *     direction = Forward and          direction = Forward and
+ *     storeMode = Columnwise:             storeMode = Rowwise:
+ *
+ *     V = (  1       )                 V = (  1 v1 v1 v1 v1 )
+ *         ( v1  1    )                     (     1 v2 v2 v2 )
+ *         ( v1 v2  1 )                     (        1 v3 v3 )
+ *         ( v1 v2 v3 )
+ *         ( v1 v2 v3 )
+ *
+ *     direction = Backward and         direction = Backward and
+ *     storeMode = Columnwise:             storeMode = Rowwise:
+ *
+ *     V = ( v1 v2 v3 )                 V = ( v1 v1  1       )
+ *         ( v1 v2 v3 )                     ( v2 v2 v2  1    )
+ *         (  1 v2 v3 )                     ( v3 v3 v3 v3  1 )
+ *         (     1 v3 )
+ *         (        1 )
+ *
+ * @tparam direction_t Either Direction or any class that implements `operator
+ * Direction()`.
+ * @tparam storage_t Either StoreV or any class that implements `operator
+ * StoreV()`.
+ *
+ * @param[in] direction
+ *     Indicates how H is formed from a product of elementary reflectors.
+ *     - Direction::Forward:  $H = H(1) H(2) ... H(k)$.
+ *     - Direction::Backward: $H = H(k) ... H(2) H(1)$.
+ *
+ * @param[in] storeMode
+ *     Indicates how the vectors which define the elementary reflectors are
+ * stored:
+ *     - StoreV::Columnwise.
+ *     - StoreV::Rowwise.
+ *
+ * @param[in] V
+ *     - If storeMode = StoreV::Columnwise: n-by-k matrix V.
+ *     - If storeMode = StoreV::Rowwise:    k-by-n matrix V.
+ *
+ * @param[in] tau Vector of length k containing the scalar factors
+ *      of the elementary reflectors H.
+ *
+ * @param[out] T Matrix of size k-by-k containing the triangular factors
+ *      of the block reflector.
+ *     - Direction::Forward:  T is upper triangular.
+ *     - Direction::Backward: T is lower triangular.
+ *
+ * @ingroup auxiliary
+ */
+template <TLAPACK_DIRECTION direction_t,
+          TLAPACK_STOREV storage_t,
+          TLAPACK_SMATRIX matrixV_t,
+          TLAPACK_VECTOR vector_t,
+          TLAPACK_SMATRIX matrixT_t>
+int larft(direction_t direction,
+          storage_t storeMode,
+          const matrixV_t& V,
+          const vector_t& tau,
+          matrixT_t& T)
+{
+    // data traits
+    using scalar_t = type_t<matrixT_t>;
+    using real_t = real_type<scalar_t>;
+    using tau_t = type_t<vector_t>;
+    using idx_t = size_type<matrixV_t>;
+
+    // using
+    using range = pair<idx_t, idx_t>;
+
+    // constants
+    const real_t one(1);
+    const real_t zero(0);
+    const idx_t n = (storeMode == StoreV::Columnwise) ? nrows(V) : ncols(V);
+    const idx_t k = size(tau);
+
+    // check arguments
+    tlapack_check_false(direction != Direction::Backward &&
+                        direction != Direction::Forward);
+    tlapack_check_false(storeMode != StoreV::Columnwise &&
+                        storeMode != StoreV::Rowwise);
+    tlapack_check_false(
+        k > ((storeMode == StoreV::Columnwise) ? ncols(V) : nrows(V)));
+    tlapack_check_false(nrows(T) < k || ncols(T) < k);
+
+    // Quick return
+    if (n == 0 || k == 0) {
+        return 0
+        // base case
+    }
+    else if (n == 1 || k == 1) {
+        t(0, 0) = tau[0];
+        return 0;
+    }
+    return 0;
+}
+
+}  // namespace tlapack
+
+#endif  // TLAPACK_LARFT_HH
